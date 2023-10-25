9a7b7fe7dfc7b5ef1d66e85725029f171157f8cb
Samuel Adeoluwa <85421643+kodezero1@users.noreply.github.com>
2023-10-25 17:34:07 +0000

    Fixed icon size on mobile devices

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -15,32 +15,32 @@ const SocialButtons: React.FC = () => {
   return (
     <div className="flex justify-center max-w-md mx-auto mt-44">
       <a href={socialProfiles.docs} target="_blank" rel="noopener noreferrer">
-        <div className="w-24 h-24 md:w-16 md:h-16 m-2 rounded-xl bg-purple-darkest bg-opacity-80 flex flex-col items-center justify-center p-2">
-          <img src={Docs} alt="Button 1" className="w-12 h-12 md:w-8 md:h-8" />
+        <div className="w-16 h-16 md:w-12 md:h-12 m-2 rounded-xl bg-purple-darkest bg-opacity-80 flex flex-col items-center justify-center p-2">
+          <img src={Docs} alt="Docs" className="w-8 h-8 md:w-6 md:h-6" />
           <div className="hidden md:block text-center text-white font-bold text-xs md:text-sm">
             Docs
           </div>
         </div>
       </a>
       <a href={socialProfiles.discord} target="_blank" rel="noopener noreferrer">
-        <div className="w-24 h-24 md:w-16 md:h-16 m-2 rounded-xl bg-purple-darkest bg-opacity-80 flex flex-col items-center justify-center p-2">
-          <img src={Discord} alt="Button 2" className="w-12 h-12 md:w-8 md:h-8" />
+        <div className="w-16 h-16 md:w-12 md:h-12 m-2 rounded-xl bg-purple-darkest bg-opacity-80 flex flex-col items-center justify-center p-2">
+          <img src={Discord} alt="Discord" className="w-8 h-8 md:w-6 md:h-6" />
           <div className="hidden md:block text-center text-white font-bold text-xs md:text-sm">
             Discord
           </div>
         </div>
       </a>
       <a href={socialProfiles.twitter} target="_blank" rel="noopener noreferrer">
-        <div className="w-24 h-24 md:w-16 md:h-16 m-2 rounded-xl bg-purple-darkest bg-opacity-80 flex flex-col items-center justify-center p-2">
-          <img src={Twitter} alt="Button 3" className="w-12 h-12 md:w-8 md:h-8" />
+        <div className="w-16 h-16 md:w-12 md:h-12 m-2 rounded-xl bg-purple-darkest bg-opacity-80 flex flex-col items-center justify-center p-2">
+          <img src={Twitter} alt="Twitter" className="w-8 h-8 md:w-6 md:h-6" />
           <div className="hidden md:block text-center text-white font-bold text-xs md:text-sm">
             Twitter
           </div>
         </div>
       </a>
       <a href={socialProfiles.github} target="_blank" rel="noopener noreferrer">
-        <div className="w-24 h-24 md:w-16 md:h-16 m-2 rounded-xl bg-purple-darkest bg-opacity-80 flex flex-col items-center justify-center p-2">
-          <img src={Github} alt="Button 4" className="w-12 h-12 md:w-8 md:h-8" />
+        <div className="w-16 h-16 md:w-12 md:h-12 m-2 rounded-xl bg-purple-darkest bg-opacity-80 flex flex-col items-center justify-center p-2">
+          <img src={Github} alt="Github" className="w-8 h-8 md:w-6 md:h-6" />
           <div className="hidden md:block text-center text-white font-bold text-xs md:text-sm">
             Github
           </div>
