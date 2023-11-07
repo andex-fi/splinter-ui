57a30be8d4b00fcac824346ab03f3a421d5fbed5
Samuel Adeoluwa <85421643+kodezero1@users.noreply.github.com>
2023-11-08 00:26:47 +0100

    Removed unused settings and added clash display font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
-  theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'dark-blue-splinter': '#0E0122',
-        'light-blue-splinter': '#120036',
-        'light-pink-splinter': '#fa98ff',
-        black: "#000000",
-        white: "#ffffff",
-        grey: "#999999",
-        "grey-light": "#6D87AC",
-        "grey-lightest": "#DFE8F9",
-        "purple-darkest": "#19102d",
-        "purple-dark": "#28133f",
-        "purple-text": "#13173E",
-        purple: "#34184B",
-        "purple-light": "#482168",
-        "purple-lightest": "#983bf6",
-      },
-      fontFamily: {
-        clashDisplay: ['var(--font-clashDisplay)'],
-      },
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				transparent: "transparent",
+				current: "currentColor",
+				"dark-blue-splinter": "#0E0122",
+				"light-blue-splinter": "#120036",
+				"light-pink-splinter": "#fa98ff",
+				"grey-spliner": "rgba(255, 255, 255, 0.16)",
+			},
+			fontFamily: {
+				clashDisplay: ["ClashDisplay-Medium", "ClashDisplay-light"],
+			},
+		},
+	},
+	plugins: [],
 };
-
