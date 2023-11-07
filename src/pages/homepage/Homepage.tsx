f305d5f1d90dab7663ea1983be54167cffaa1167
Samuel Adeoluwa <85421643+kodezero1@users.noreply.github.com>
2023-11-08 00:22:38 +0100

    Added planet images and fixed formatting

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
-// import TopGalaxy from "../../assets/images/top-galaxy.png";
-// import TopLeftPlanet from "../../assets/images/right-blue-planet.png";
-// import LeftPlanet from "../../assets/images/left-pink-planet.png";
-// import BottomPlanet from "../../assets/images/bottom-planet.png";
+import TopLeftPlanet from '../../assets/images/top-left-planet.png'
+import TopGalaxy from "../../assets/images/top-galaxy.png";
+import rightBluePlanet from "../../assets/images/right-blue-planet.png";
+import LeftPlanet from "../../assets/images/left-pink-planet.png";
+import BottomPlanet from "../../assets/images/bottom-planet.png";
 
 import SocialButtons from "../../components/SocialButtons";
 
@@ -13,37 +14,36 @@ const Home: FC = () => {
     <>
       <section className="bg-big text-center text-white px-4 md:px-10 lg:px-20 py-10 md:pt-24 pb-0 w-full">
         <div className="sm:mt-7 md:mt-7">
-          {/* <div className="galaxy">
+          <div className="top-planet">
+            <img src={TopLeftPlanet} alt="pink planet" height={50} width={50} />
+          </div>
+
+          <div className="galaxy">
             <img src={TopGalaxy} alt="Galaxy" height={100} width={100} />
-          </div> */}
+          </div>
           <h1 className="text-3xl lg:text-7xl font-extrabold mb-2 hero-spacing">
             First Perpetual Decentralized <br />
             Exchange on Flare
           </h1>
+          <div className="absolute w-24 h-24 blue-planet">
+            <img src={rightBluePlanet} alt="blue planet" />
+          </div>
         </div>
 
         <div className="mt-8">
-          <p className="mb-1 text-md font-bold opacity-80">
+          <p className="mb-1 text-spacing opacity-80">
             Trade BTC, Eth, FLARE, and other top cryptocurrencies with up to 50x
           </p>
-          <p className="mb-1 text-md font-bold opacity-80">
+          <p className="mb-1 text-spacing opacity-80">
             leverage directly from your wallet, prepare yourself for a
             revolutionary
           </p>
-          <p className="mb-1 text-md font-bold opacity-80">
+          <p className="mb-1 text-spacing opacity-80">
             leap in decentralized finance. Splinter is your gateway to unrivaled
           </p>
-          <p className="mb-1 text-md font-bold opacity-80">
+          <p className="mb-1 text-spacing opacity-80">
             trading experiences that combine speed, security, and innovation
           </p>
-
-          {/* <div className="absolute w-24 h-24 top-35 right-0">
-            <img src={TopLeftPlanet} alt="TopLeftPlanet" />
-          </div>
-
-          <div className="absolute w-24 h-24 top-35 left-0 ">
-            <img src={LeftPlanet} alt="LeftPlanet" />
-          </div> */}
         </div>
 
         <div className="flex items-center gap-4 justify-center mt-8">
@@ -59,10 +59,13 @@ const Home: FC = () => {
             Learn more
           </Button>
         </div>
+        <div className="absolute left-planet ">
+          <img src={LeftPlanet} alt="LeftPlanet" />
+        </div>
 
-        {/* <div className="mt-14 absolute w-24 h-24 top-25 right-40 ">
+        <div className="mt-14 absolute lg:hidden md:w-15 md:h-7 sm:w-15 sm:h-7 w-24 h-24 top-25 right-40 ">
             <img src={BottomPlanet} alt="BottomPlanet" />
-        </div> */}
+        </div>
 
         <div className="sm:mt-16">
           <SocialButtons />
